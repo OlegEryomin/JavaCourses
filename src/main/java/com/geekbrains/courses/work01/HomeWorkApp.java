6b9663a1b21087538a556760ee79a610fad010ac
Еремин Олег <olegeryomin@icloud.com>
2022-04-12 01:07:56 +0300

    Создал метод printThreeWords()

--- src/main/java/com/geekbrains/courses/work01/HomeWorkApp.java
+++ src/main/java/com/geekbrains/courses/work01/HomeWorkApp.java
@@ -4,5 +4,13 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
 
+        printThreeWords("Orange");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
+
+    }
+
+    private static void printThreeWords(String fruit) {
+        System.out.println(fruit);
     }
 }
