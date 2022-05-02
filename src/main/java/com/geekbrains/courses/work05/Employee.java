bddda53fd9cef0bc77fecb4caf7f4125930f43d6
Еремин Олег <olegeryomin@icloud.com>
2022-05-02 15:16:20 +0300

    2. Конструктор класса должен заполнять эти поля при создании объекта.

--- src/main/java/com/geekbrains/courses/work05/Employee.java
+++ src/main/java/com/geekbrains/courses/work05/Employee.java
@@ -9,4 +9,14 @@ public class Employee {
     String phone;
     BigDecimal salary;
     int age;
+
+    public Employee(String fullName, String position, String email, String phone, BigDecimal salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 }
+
