892abd294cdfba1c6327a94b2004fac7fe641a77
Еремин Олег <olegeryomin@icloud.com>
2022-05-02 15:48:02 +0300

    4. Создать массив из 5 сотрудников

--- src/main/java/com/geekbrains/courses/work05/Main.java
+++ src/main/java/com/geekbrains/courses/work05/Main.java
@@ -12,5 +12,32 @@ public class Main {
                                     new BigDecimal(5000), 21);
 
         System.out.println(em.toString());
+
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Eryomin Oleg Aleksandrovich",
+                                    "developer",
+                                    "eremin_o@bsu.edu.ru",
+                                    "+7-920-555-55-55",
+                                    new BigDecimal(5000), 21);
+        employees[1] = new Employee("Shubin Maxim Sergeevich",
+                                    "developer",
+                                    "sasbaa@ya.ru",
+                                    "+7-920-555-55-56",
+                                    new BigDecimal(5000), 22);
+        employees[2] = new Employee("Komov Pavel Evgenevich",
+                                    "sheriff",
+                                    "2121312@ya.ru",
+                                    "+7-920-555-55-57",
+                                    new BigDecimal(1000), 21);
+        employees[3] = new Employee("Nechaev Vlad Aleksandrovich",
+                                    "security guard",
+                                    "nechaev@ya.ru",
+                                    "+7-920-555-55-58",
+                                    new BigDecimal(1000), 20);
+        employees[4] = new Employee("Glazkov Roman Aleksandrovich",
+                                    "engineer",
+                                    "glazkov@ya.ru",
+                                    "+7-920-555-55-59",
+                                    new BigDecimal(5000), 22);
     }
 }
