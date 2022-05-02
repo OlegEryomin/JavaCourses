d11750eeb0577cae1eb5e6138c716dd04de5fb1b
Еремин Олег <olegeryomin@icloud.com>
2022-05-02 15:15:18 +0300

    Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст

---
+++ src/main/java/com/geekbrains/courses/work05/Employee.java
@@ -0,0 +1,12 @@
+package com.geekbrains.courses.work05;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    String fullName;
+    String position;
+    String email;
+    String phone;
+    BigDecimal salary;
+    int age;
+}
