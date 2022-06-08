e44941c227e6cdadd84ea6d07160d7493f02302c
Еремин Олег <olegeryomin@icloud.com>
2022-06-08 21:26:01 +0300

    Задание 1

--- src/main/java/com/geekbrains/courses/work10/Main.java
+++ src/main/java/com/geekbrains/courses/work10/Main.java
@@ -1,4 +1,36 @@
 package com.geekbrains.courses.work10;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Spring");
+        list.add("универсальный");
+        list.add("фреймворк");
+        list.add("Java");
+        list.add("Spring");
+        list.add("обеспечивает");
+        list.add("решения");
+        list.add("многих");
+        list.add("задач");
+        list.add("Java");
+
+        Set<String> set = new HashSet<>(list);
+        System.out.println(set);
+        for (String s : set) {
+            System.out.println("Слово: " + s);
+            int i = 0;
+            for (String l : list) {
+                if (s == l) {
+                    i++;
+                }
+            }
+            System.out.println("Встречается: " + i + " раз(а)");
+        }
+
+    }
 }
